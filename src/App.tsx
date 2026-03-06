4c00584b1524a97deb527d346a7ba605072b796b
emkuwa <84395152+emkuwa@users.noreply.github.com>
2026-03-06 08:43:31 +0300

    Import new page components for property listings

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import BuyPropertyZanzibar from "./pages/BuyPropertyZanzibar";
+import PajeVillas from "./pages/PajeVillas";
+import NungwiBeachfront from "./pages/NungwiBeachfront";
 
 const queryClient = new QueryClient();
 
