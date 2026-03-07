c5ecac4c93a6dfd16a74daba4948fe4c3d9abbf5
emkuwa <84395152+emkuwa@users.noreply.github.com>
2026-03-07 04:30:46 +0300

    Create propertyTypes.ts

---
+++ src/data/propertyTypes.ts
@@ -0,0 +1,12 @@
+export const propertyTypes = [
+  "villa",
+  "beachfront-villa",
+  "hotel",
+  "boutique-hotel",
+  "resort",
+  "apartment",
+  "holiday-rental",
+  "beachfront-property",
+  "land",
+  "tourism-land"
+];
